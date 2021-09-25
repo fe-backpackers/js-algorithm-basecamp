81a51f6d9ad1c2e6d0a13d3cbc26c2784ea5b176
bigsaigon333 <likepeppermint@gmail.com>
2021-09-25 18:22:17 +0900

    docs(DFS/BFS): 이코테 05장 2. 탐색 알고리즘 DFS/BFS 예제를 JS로 구현한다

---
+++ src/js-for-coding-test/PART-02/05-DFS_BFS/06.js
@@ -0,0 +1,8 @@
+// 2차원 배열을 이용해 인접 행렬 표현
+const graph = [
+  [0, 7, 5],
+  [7, 0, Infinity],
+  [5, Infinity, 0],
+];
+
+console.log(graph); // [ [ 0, 7, 5 ], [ 7, 0, Infinity ], [ 5, Infinity, 0 ] ]
