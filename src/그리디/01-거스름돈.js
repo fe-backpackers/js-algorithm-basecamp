02741ec936d544a66a80e65a66d7e6672e8580ea
moonheekim0118 <hellomooneekim@gmail.com>
2021-09-28 09:14:42 +0900

    chore : 프리티어 적용

--- src/그리디/01-거스름돈.js
+++ src/그리디/01-거스름돈.js
@@ -1,32 +1,28 @@
-
-// n : 거스름돈
-
 // 내가 푼 방법
 
-function solution(n){
-    let count = 0;
-    const units = [500,100,50,10];
-    let index = 0;
-    while(n > 0 && index < 4){
-        if(units[index] > n){
-            index++;
-        }
-        n-=units[index];
-        count++;
+function solution(n) {
+  let count = 0;
+  const units = [500, 100, 50, 10];
+  let index = 0;
+  while (n > 0 && index < 4) {
+    if (units[index] > n) {
+      index++;
     }
-    return count;
+    n -= units[index];
+    count++;
+  }
+  return count;
 }
 
 // 책에 나와있는 답안
 
-function solution2(n){
-    let count = 0;
-    [500,100,50,10].forEach((coin)=>{
-        count += (Math.floor(n/coin));
-        n %= coin;
-    });
-    return count;
-
+function solution2(n) {
+  let count = 0;
+  [500, 100, 50, 10].forEach((coin) => {
+    count += Math.floor(n / coin);
+    n %= coin;
+  });
+  return count;
 }
 
 console.log(solution2(1260));
