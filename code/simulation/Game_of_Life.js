fe-backpackers/js-algorithm-basecamp

commit bc9e9f9770f9f740b0ed4ae156cc0d3d1ba20334

♻️ Divide into partial modules (utils.js)

lib/util_modules/array.js
@@ -1,0 +1,10 @@
+/**
+ * @description 배열 안의 모든 값이 동일한지 비교한다 (얕은비교)
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Boolean}
+ */
+exports.isSame = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+  return arr1.every((v, i) => v === arr2[i]);
+};

lib/util_modules/input.js
@@ -1,0 +1,53 @@
+const fs = require("fs");
+const fsPromises = require("fs/promises");
+const readline = require("readline");
+
+/**
+ * @param {string} dir 읽을 파일 경루
+ * @return {arrray[string]} 파일의 한 문장씩을 담은 문자열 배열
+ */
+exports.inputByFile = async (dir) => {
+  const file = await fsPromises.readFile(dir, "utf-8");
+  return file.split(/\r?\n/); // window & mac
+};
+
+/**
+ * @param {string} dir 읽을 파일 경로
+ * @return {string} line 파일에서 순차적으로 한 줄씩 읽은 것
+ * @example
+ *  const reader = exports.lineReader("input.txt");
+    console.log(reader()); // 한줄씩 읽어온다
+ */
+exports.lineReader = (dir) => {
+  const file = fs.readFileSync(dir, "utf8").split(/\r?\n/);
+  let line = 0;
+  return () => file[line++];
+};
+
+/**
+ * @description 콘솔에서 한 줄씩 입력받아 generator 안에 넣어주는 함수. {string}
+ * @param {generator} solution 함수. yield로 input을 건네받는다
+ * @return {void}
+ * @see https://www.codecademy.com/articles/getting-user-input-in-node-js
+ * @usage readConsole(generatorExample)
+ * @example generatorExample 함수 참고
+ */
+exports.readConsole = (generator) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const gen = generator();
+  gen.next(); // to wait console input
+
+  rl.on("line", (line) => {
+    if (gen.next(line).done) rl.close();
+  });
+};
+
+const generatorExample = function* () {
+  const n = yield; // 콘솔에서 입력받은 한 줄 저장
+  const m = yield; // 콘솔에서 입력받은 한 줄 저장
+  return Number(n) + Number(m);
+};

lib/util_modules/math.js
@@ -1,0 +1,31 @@
+/**
+ * @description 파라미터로 받은 모든 수의 합을 구해 리턴한다
+ * @param {array} 더할 수
+ * @return {Number} 수의 합
+ * @usage utils.sum(1,2,3,4)
+ */
+exports.sum = (...nums) => {
+  return nums.reduce((prev, cur) => prev + cur, 0);
+};
+
+/**
+ * @description num이 [min, max]를 벗어나는 경우 각각 min, max를 리턴하고, 그 외엔 num을 리턴한다.
+ * @param {Number} num
+ * @param {Number} min
+ * @param {Number} max
+ * @return {Number}
+ */
+exports.wrap = (num, min, max) => {
+  return Math.min(Math.max(num, min), max);
+};
+
+/**
+ * @description num이 [min, max] 범위 안에 있는지 리턴한다.
+ * @param {Number} num
+ * @param {Number} min
+ * @param {Number} max
+ * @return {Boolean}
+ */
+exports.isInRange = (num, min, max) => {
+  return num >= min && num <= max;
+};

lib/util_modules/string.js
@@ -1,0 +1,10 @@
+/**
+ * @description 문자 a에서 b까지 유니코드상의 거리를 리턴.
+ * @param {Char} a
+ * @param {Char} b
+ * @example charDistance('a', 'b') = 1, charDistance('c', 'b') = -1;
+ * @return {Number}
+ */
+exports.unicodeDistance = (a, b) => {
+  return b.charCodeAt(0) - a.charCodeAt(0);
+};

lib/utils.js
@@ -4,110 +4,14 @@
  * @see https://nodejs.org/api/fs.html#fs_file_system
  */
 
-const fs = require("fs");
-const fsPromises = require("fs/promises");
-const readline = require("readline");
+const input = require("./util_modules/input.js");
+const math = require("./util_modules/math.js");
+const string = require("./util_modules/string.js");
+const array = require("./util_modules/array.js");
 
-/**
- * @param {string} dir 읽을 파일 경루
- * @return {arrray[string]} 파일의 한 문장씩을 담은 문자열 배열
- */
-exports.inputByFile = async (dir) => {
-  const file = await fsPromises.readFile(dir, "utf-8");
-  return file.split(/\r?\n/); // window & mac
-};
-
-/**
- * @param {string} dir 읽을 파일 경로
- * @return {string} line 파일에서 순차적으로 한 줄씩 읽은 것
- * @example
- *  const reader = exports.lineReader("input.txt");
-    console.log(reader()); // 한줄씩 읽어온다
- */
-exports.lineReader = (dir) => {
-  const file = fs.readFileSync(dir, "utf8").split(/\r?\n/);
-  let line = 0;
-  return () => file[line++];
-};
-
-/**
- * @description 콘솔에서 한 줄씩 입력받아 generator 안에 넣어주는 함수. {string}
- * @param {generator} solution 함수. yield로 input을 건네받는다
- * @return {void}
- * @see https://www.codecademy.com/articles/getting-user-input-in-node-js
- * @usage readConsole(generatorExample)
- * @example generatorExample 함수 참고
- */
-exports.readConsole = (generator) => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  const gen = generator();
-  gen.next(); // to wait console input
-
-  rl.on("line", (line) => {
-    if (gen.next(line).done) rl.close();
-  });
-};
-
-const generatorExample = function* () {
-  const n = yield; // 콘솔에서 입력받은 한 줄 저장
-  const m = yield; // 콘솔에서 입력받은 한 줄 저장
-  return Number(n) + Number(m);
-};
-
-/**
- * @description 파라미터로 받은 모든 수의 합을 구해 리턴한다
- * @param {array} 더할 수
- * @return {Number} 수의 합
- * @usage utils.sum(1,2,3,4)
- */
-exports.sum = (...nums) => {
-  return nums.reduce((prev, cur) => prev + cur, 0);
-};
-
-/**
- * @description num이 [min, max]를 벗어나는 경우 각각 min, max를 리턴하고, 그 외엔 num을 리턴한다.
- * @param {Number} num
- * @param {Number} min
- * @param {Number} max
- * @return {Number}
- */
-exports.wrap = (num, min, max) => {
-  return Math.min(Math.max(num, min), max);
-};
-
-/**
- * @description num이 [min, max] 범위 안에 있는지 리턴한다.
- * @param {Number} num
- * @param {Number} min
- * @param {Number} max
- * @return {Boolean}
- */
-exports.isInRange = (num, min, max) => {
-  return num >= min && num <= max;
-};
-
-/**
- * @description 문자 a에서 b까지 유니코드상의 거리를 리턴.
- * @param {Char} a
- * @param {Char} b
- * @example charDistance('a', 'b') = 1, charDistance('c', 'b') = -1;
- * @return {Number}
- */
-exports.unicodeDistance = (a, b) => {
-  return b.charCodeAt(0) - a.charCodeAt(0);
-};
-
-/**
- * @description 배열 안의 모든 값이 동일한지 비교한다 (얕은비교)
- * @param {Array} arr1
- * @param {Array} arr2
- * @return {Boolean}
- */
-exports.isSame = (arr1, arr2) => {
-  if (arr1.length !== arr2.length) return false;
-  return arr1.every((v, i) => v === arr2[i]);
+module.exports = {
+  input,
+  math,
+  string,
+  array,
 };

commit d89f994df1526c53c89f99d0da592c2915b1e419

[2주차][구현][Sunmon] 학습한 내용 정리입니다 (#26)
* 📝 Add note (simulation)

* 👔 Add util function (wrap)

* 👔 Add util functions (inRange, unicodeDist, isSame)

* 🚩 Add exercises (simulation)

* ♻️ Divide into partial modules (utils.js)

* :adhesive_bandage: Fix variable names

- Fixed the variable name mismatch caused by bc9e9f9770f9f740b0ed4ae156cc0d3d1ba20334

* 🚩 Add previous questions (simulation 07~09, 12)

🚧 Add previous questions (simulation 12)

* 🚩Add leetcode (simulation)

* Update dongbin-book/04-Simulation/04-1.js

Co-authored-by: Danny Kim <likepeppermint@gmail.com>

Co-authored-by: Danny Kim <likepeppermint@gmail.com>

code/simulation/Game_of_Life.js
@@ -1,0 +1,42 @@
+/**
+ * @name Game of Life
+ * @link https://leetcode.com/problems/game-of-life/
+ * @date 2021-10-10
+ * @author sunmon
+ * @difficulty medium
+ */
+
+/**
+ * @param {number[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
+ */
+var gameOfLife = function (board) {
+  const [m, n] = [board.length, board[0].length];
+  const expandBoard = Array.from(Array(m + 2), () => Array(n + 2).fill(0));
+
+  // wrap around board using 0
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      expandBoard[i + 1][j + 1] = board[i][j];
+    }
+  }
+
+  // get next state
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      let neighbors = -expandBoard[i][j];
+      for (let x = -1; x <= 1; x++) {
+        for (let y = -1; y <= 1; y++) {
+          neighbors += expandBoard[x + i][y + j];
+        }
+      }
+
+      if (expandBoard[i][j] === 1) {
+        if (neighbors < 2) board[i - 1][j - 1] = 0;
+        else if (neighbors > 3) board[i - 1][j - 1] = 0;
+      } else {
+        if (neighbors === 3) board[i - 1][j - 1] = 1;
+      }
+    }
+  }
+};

code/simulation/Sprial_Matrix.js
@@ -1,0 +1,43 @@
+/**
+ * @name Sprial Matrix
+ * @link https://leetcode.com/problems/spiral-matrix/
+ * @date 2021-10-10
+ * @author sunmon
+ * @difficult medium
+ */
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function (matrix) {
+  const dir = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0],
+  ];
+  const [m, n] = [matrix.length, matrix[0].length];
+  const answer = [];
+  let [x, y] = [0, 0];
+  let d = 0;
+  while (answer.length < m * n) {
+    if (matrix[x][y] != 999) {
+      answer.push(matrix[x][y]);
+      matrix[x][y] = 999;
+    }
+    const [nx, ny] = [x + dir[d][0], y + dir[d][1]];
+
+    if (nx < 0 || nx >= m || ny < 0 || ny >= n) {
+      d = (d + 1) % 4;
+      continue;
+    }
+    if (matrix[nx][ny] === 999) {
+      d = (d + 1) % 4;
+      continue;
+    }
+    [x, y] = [nx, ny];
+  }
+
+  return answer;
+};

code/simulation/Watter_bottles.js
@@ -1,0 +1,23 @@
+/**
+ * @name Watter bottles
+ * @link https://leetcode.com/problems/water-bottles/
+ * @date 2021-10-10
+ * @author sunmon
+ * @difficulty easy
+ */
+
+/**
+ * @param {number} numBottles
+ * @param {number} numExchange
+ * @return {number}
+ */
+var numWaterBottles = function (numBottles, numExchange) {
+  let drink = numBottles;
+  while (numBottles >= numExchange) {
+    const fullBottles = parseInt(numBottles / numExchange);
+    drink += fullBottles;
+    numBottles = fullBottles + (numBottles % numExchange);
+  }
+
+  return drink;
+};

dongbin-book/04-Simulation/04-1.js
@@ -1,0 +1,29 @@
+/**
+ * @name 상하좌우
+ * @link 동빈북 4-1
+ * @date 2021-10-07
+ * @author sunmon
+ */
+
+const Utils = require("../../lib/utils");
+
+const generator = function* () {
+  const n = yield; // 콘솔에서 입력받은 한 줄 저장
+  const m = yield; // 콘솔에서 입력받은 한 줄 저장
+  const answer = solution(n, m);
+  console.log(answer);
+};
+
+const solution = (n, m) => {
+  const dir = { R: [0, 1], L: [0, -1], U: [-1, 0], D: [1, 0] };
+
+  return m.split(" ").reduce(
+    (prev, cur) => ([
+        Utils.math.wrap(prev[0] + dir[cur][0], 1, +n),
+        Utils.math.wrap(prev[1] + dir[cur][1], 1, +n),
+      ])
+    [1, 1]
+  );
+};
+
+Utils.input.readConsole(generator);

dongbin-book/04-Simulation/04-2.js
@@ -1,0 +1,29 @@
+/**
+ * @name 시각
+ * @link 동빈북 4-2
+ * @date 2021-10-07
+ * @author sunmon
+ */
+
+const Utils = require("../../lib/utils");
+
+const generator = function* () {
+  const n = yield; // 콘솔에서 입력받은 한 줄 저장
+  const answer = solution(n);
+  console.log(answer);
+};
+
+const solution = (n) => {
+  let counter = 0;
+  for (let i = 0; i <= +n; i++) {
+    for (let j = 0; j < 60; j++) {
+      for (let k = 0; k < 60; k++) {
+        if (("" + i + j + k).includes("3")) counter += 1;
+      }
+    }
+  }
+
+  return counter;
+};
+
+Utils.input.readConsole(generator);

dongbin-book/04-Simulation/04-3.js
@@ -1,0 +1,34 @@
+/**
+ * @name 왕실의 나이트
+ * @link 동빈북 4-3
+ * @date 2021-10-08
+ * @author sunmon
+ */
+
+const Utils = require("../../lib/utils");
+
+const generator = function* () {
+  const n = yield; // 콘솔에서 입력받은 한 줄 저장
+  const answer = solution(n);
+  console.log(answer);
+};
+
+const solution = (n) => {
+  const [col, row] = [Utils.string.unicodeDistance("a", n[0]), +n[1]];
+  return [
+    [1, 2],
+    [1, -2],
+    [-1, 2],
+    [-1, -2],
+    [2, 1],
+    [2, -1],
+    [-2, 1],
+    [-2, -1],
+  ].filter(
+    (d) =>
+      Utils.math.isInRange(col + d[0], 1, 8) &&
+      Utils.math.isInRange(row + d[1], 1, 8)
+  ).length;
+};
+
+Utils.input.readConsole(generator);

dongbin-book/04-Simulation/04-4.js
@@ -1,0 +1,56 @@
+/**
+ * @name 게임 개발
+ * @link 동빈북 4-4
+ * @date 2021-10-08
+ * @author sunmon
+ */
+
+const Utils = require("../../lib/utils");
+
+const generator = function* () {
+  const input = yield; // 콘솔에서 입력받은 한 줄 저장
+  const charactor = yield;
+  const [n, m] = input.split(" ").map(Number);
+  const board = []; // board[x][y] 형태
+  for (let i = 0; i < n; i++) {
+    const temp = yield;
+    board.push(temp.split(" "));
+  }
+  const answer = solution(n, m, charactor, board);
+  console.log(answer);
+};
+
+const solution = (n, m, charactor, board) => {
+  const dx = [-1, 0, 1, 0]; // 북 동 남 서
+  const dy = [0, 1, 0, -1]; // 북 동 남 서
+  let [x, y, dir] = charactor.split(" ").map(Number);
+  let counter = 0;
+
+  while (true) {
+    const cur = [x, y, dir];
+    if (board[x][y] !== -1) {
+      counter += 1;
+      board[x][y] = -1; // 방문한곳은 -1로 표기
+    }
+
+    // 왼쪽으로 회전하며 갈 수 있는지 본다
+    for (let i = 0; i < 4; i++) {
+      dir = [dir - 1 + 4] % 4; // 왼쪽으로 돈다
+      const [nx, ny] = [x + dx[dir], y + dy[dir]];
+      if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
+      if (board[nx][ny] === "1" || board[nx][ny] === -1) continue;
+      [x, y] = [nx, ny];
+      break;
+    }
+
+    // 4방향 다 가지 못하고 원래대로 돌아온 경우는 뒤로 한칸 이동
+    if (Utils.array.isSame(cur, [x, y, dir])) {
+      [x, y] = [x - dx[dir], y - dy[dir]];
+      if (board[x][y] === "1") break;
+    }
+  }
+
+  return counter;
+};
+
+Utils.input.readConsole(generator);

dongbin-book/12-Simulation/07.js
@@ -1,0 +1,31 @@
+/**
+ * @name 럭키 스트레이트
+ * @link 동빈북 12-1
+ * @date 2021-10-09
+ * @ref
+ * @author sunmon
+ * @description 접근방법:
+ *  1. 문자열의 왼쪽 / 오른쪽 절반으로 나눈다
+ *  2. 왼쪽과 오른쪽의 각 자리수의 합이 같으면 LUCKY, 아님녀 READY 리턴
+ */
+
+const Utils = require("../../lib/utils");
+
+const solution = (s) => {
+  const mid = s.length / 2;
+  let left = 0,
+    right = 0;
+  for (let i = 0; i < s.length / 2; i++) {
+    left += +s[i];
+    right += +s[i + mid];
+  }
+  return left === right ? "LUCKY" : "READY";
+};
+
+const generator = function* () {
+  const s = yield; // 콘솔에서 입력받은 한 줄 저장
+  const answer = solution(s);
+  console.log(answer);
+};
+
+Utils.input.readConsole(generator);

dongbin-book/12-Simulation/08.js
@@ -1,0 +1,32 @@
+/**
+ * @name 문자열 재정렬
+ * @link 동빈북 12-2
+ * @date 2021-10-09
+ * @ref
+ * @author sunmon
+ * @description 접근방법:
+ *  1. 입력한 문자열을 순차적으로 돌며 다음 작업을 수행한다
+ *    - 현재 문자가 문자열이면 str에 덧붙여 저장한다
+ *    - 현재 문자가 숫자라면 숫자의 합에 더한다
+ *  2. str을 오름차순으로 정렬한 후 숫자를 더해 리턴한다
+ */
+
+const Utils = require("../../lib/utils");
+
+const solution = (s) => {
+  let str = "";
+  let num = 0;
+  for (let i = 0; i < s.length; i++) {
+    isNaN(s[i]) ? (str += s[i]) : (num += +s[i]);
+  }
+
+  return [...str].sort().join("") + num;
+};
+
+const generator = function* () {
+  const s = yield; // 콘솔에서 입력받은 한 줄 저장
+  const answer = solution(s);
+  console.log(answer);
+};
+
+Utils.input.readConsole(generator);

dongbin-book/12-Simulation/09.js
@@ -1,0 +1,48 @@
+/**
+ * @name 문자열 압축
+ * @link 동빈북 12-3
+ * @date 2021-10-09
+ * @ref https://programmers.co.kr/learn/courses/30/lessons/60057
+ * @author sunmon
+ * @description 접근방법:
+ *  1.주어진 문자열 s를 순회하며 다음 동작을 수행한다
+ *  - 시작 인덱스 i, 문자열을 자르는 길이 l, 반복되는 값 count, 압축한 총 길이를 result 라고 정의하고
+ *  - a. 문자열 [i, i+l] === [i+l, i+2l]이라면 count += 1 후 i += l 을 수행
+ *  - b. 문자열 [i, i+l] !== [i+l, i+2l]이라면 result += l, i += l을 수행하고
+ *       count > 1 이라면 count의 글자 길이만큼 result에 더한다.
+ *       그 후 count = 1로 초기화한다.
+ *  2. i의 범위를 [0, s.length-l)에 대해 모두 순회했다면 남은 문자열(s[s.length-l-1, s.length))에 대해 1.a혹은 1.b을 수행한다
+ *  - 이때 남은 문자열의 길이 <=l 임에 주의한다.
+ *  3. 크기가 [1, s.length/2] 인 l에 대해 1,2를 수행하여 가장 길이가 작은 result의 값을 리턴한다
+ */
+
+const assert = require("assert");
+
+const solution = (s) => {
+  let ret = s.length;
+  for (let l = 1; l <= s.length / 2; l++) {
+    let result = 0;
+    let count = 1;
+    let i = 0;
+    while (i < s.length - l) {
+      if (s.slice(i, i + l) === s.slice(i + l, i + l + l)) {
+        count += 1;
+      } else {
+        if (count > 1) result += ("" + count).length;
+        result += l;
+        count = 1;
+      }
+      i += l;
+    }
+    if (count > 1) result += ("" + count).length;
+    result += s.length - i;
+    ret = Math.min(ret, result);
+  }
+  return ret;
+};
+
+assert.strictEqual(solution("aabbaccc"), 7);
+assert.strictEqual(solution("ababcdcdababcdcd"), 9);
+assert.strictEqual(solution("abcabcdede"), 8);
+assert.strictEqual(solution("abcabcabcabcdededededede"), 14);
+assert.strictEqual(solution("xababcdcdababcdcd"), 17);

dongbin-book/12-Simulation/12.js
@@ -1,0 +1,131 @@
+/**
+ * @name 기둥과 보 설치
+ * @link 동빈북 12-6
+ * @date 2021-10-10
+ * @ref https://programmers.co.kr/learn/courses/30/lessons/60061
+ * @author sunmon
+ * @description 접근방법:
+ * 1. build_frame의 원소를 [x,y,a,b]라고 하고, 설치한 배열을 저장하는 변수를 built라고 한다.
+ * 2. if b == 1 : //설치
+ *      add [x,y,a] in built
+ *      if not isValid(built) : pop [x,y,a] in built
+ * 3. if b === 0: //삭제
+ *      pop [x,y,a] in built
+ *      if not isValid(built): push [x,y,a] in built
+ * 4. func isValid:
+ *     if a==0:  // 기둥설치
+ *       if every [x,y,z] of built:
+ *         ([x,y,1] || [x-1,y,1] || [x,y-1,0]) in built || y == 0 ) return True
+ *     if a==1: // 보 설치
+ *       if every [x,y,z] of built:
+ *         ([x,y-1,0] || [x+1,y-1,0] ) in built || ([x-1,y,1] && [x+1,y,1]) in built : return True
+ * 5. built를 오름차순 정렬하여 리턴한다.
+ */
+
+const assert = require("assert");
+
+const solution = (n, build_frame) => {
+  let built = [];
+  for (const [x, y, a, b] of build_frame) {
+    if (b === 0) {
+      // 삭제
+      const next = built.filter((el) => !isSame(el, [x, y, a]));
+      if (next.every((el) => isValid(...el, next))) {
+        built = next;
+      }
+    } else {
+      // 설치
+      if (isValid(x, y, a, built)) {
+        built.push([x, y, a]);
+      }
+    }
+  }
+
+  return built.sort((a, b) => {
+    return a[0] < b[0]
+      ? -1
+      : a[0] > b[0]
+      ? 1
+      : a[1] < b[1]
+      ? -1
+      : a[1] > b[1]
+      ? 1
+      : a[2] < b[2]
+      ? -1
+      : a[2] > b[2]
+      ? 1
+      : 0;
+  });
+};
+
+const isValid = (x, y, a, built) => {
+  if (a === 0) {
+    // 기둥
+    if (includes(built, [x, y, 1]) || includes(built, [x - 1, y, 1]))
+      return true;
+    if (includes(built, [x, y - 1, 0])) return true;
+    if (y === 0) return true;
+  } else if (a === 1) {
+    // 보
+    if (includes(built, [x, y - 1, 0]) || includes(built, [x + 1, y - 1, 0]))
+      return true;
+    if (includes(built, [x + 1, y, 1]) && includes(built, [x - 1, y, 1]))
+      return true;
+  }
+
+  return false;
+};
+
+const includes = (arr, elem) => {
+  return arr.some((el) => isSame(el, elem));
+};
+
+const isSame = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+  return arr1.every((v, i) => v === arr2[i]);
+};
+
+assert.deepStrictEqual(
+  solution(5, [
+    [1, 0, 0, 1],
+    [1, 1, 1, 1],
+    [2, 1, 0, 1],
+    [2, 2, 1, 1],
+    [5, 0, 0, 1],
+    [5, 1, 0, 1],
+    [4, 2, 1, 1],
+    [3, 2, 1, 1],
+  ]),
+  [
+    [1, 0, 0],
+    [1, 1, 1],
+    [2, 1, 0],
+    [2, 2, 1],
+    [3, 2, 1],
+    [4, 2, 1],
+    [5, 0, 0],
+    [5, 1, 0],
+  ]
+);
+assert.deepStrictEqual(
+  solution(5, [
+    [0, 0, 0, 1],
+    [2, 0, 0, 1],
+    [4, 0, 0, 1],
+    [0, 1, 1, 1],
+    [1, 1, 1, 1],
+    [2, 1, 1, 1],
+    [3, 1, 1, 1],
+    [2, 0, 0, 0],
+    [1, 1, 1, 0],
+    [2, 2, 0, 1],
+  ]),
+  [
+    [0, 0, 0],
+    [0, 1, 1],
+    [1, 1, 1],
+    [2, 1, 1],
+    [3, 1, 1],
+    [4, 0, 0],
+  ]
+);

lib/util_modules/array.js
@@ -1,0 +1,10 @@
+/**
+ * @description 배열 안의 모든 값이 동일한지 비교한다 (얕은비교)
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Boolean}
+ */
+exports.isSame = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) return false;
+  return arr1.every((v, i) => v === arr2[i]);
+};

lib/util_modules/input.js
@@ -1,0 +1,53 @@
+const fs = require("fs");
+const fsPromises = require("fs/promises");
+const readline = require("readline");
+
+/**
+ * @param {string} dir 읽을 파일 경루
+ * @return {arrray[string]} 파일의 한 문장씩을 담은 문자열 배열
+ */
+exports.inputByFile = async (dir) => {
+  const file = await fsPromises.readFile(dir, "utf-8");
+  return file.split(/\r?\n/); // window & mac
+};
+
+/**
+ * @param {string} dir 읽을 파일 경로
+ * @return {string} line 파일에서 순차적으로 한 줄씩 읽은 것
+ * @example
+ *  const reader = exports.lineReader("input.txt");
+    console.log(reader()); // 한줄씩 읽어온다
+ */
+exports.lineReader = (dir) => {
+  const file = fs.readFileSync(dir, "utf8").split(/\r?\n/);
+  let line = 0;
+  return () => file[line++];
+};
+
+/**
+ * @description 콘솔에서 한 줄씩 입력받아 generator 안에 넣어주는 함수. {string}
+ * @param {generator} solution 함수. yield로 input을 건네받는다
+ * @return {void}
+ * @see https://www.codecademy.com/articles/getting-user-input-in-node-js
+ * @usage readConsole(generatorExample)
+ * @example generatorExample 함수 참고
+ */
+exports.readConsole = (generator) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const gen = generator();
+  gen.next(); // to wait console input
+
+  rl.on("line", (line) => {
+    if (gen.next(line).done) rl.close();
+  });
+};
+
+const generatorExample = function* () {
+  const n = yield; // 콘솔에서 입력받은 한 줄 저장
+  const m = yield; // 콘솔에서 입력받은 한 줄 저장
+  return Number(n) + Number(m);
+};

lib/util_modules/math.js
@@ -1,0 +1,31 @@
+/**
+ * @description 파라미터로 받은 모든 수의 합을 구해 리턴한다
+ * @param {array} 더할 수
+ * @return {Number} 수의 합
+ * @usage utils.sum(1,2,3,4)
+ */
+exports.sum = (...nums) => {
+  return nums.reduce((prev, cur) => prev + cur, 0);
+};
+
+/**
+ * @description num이 [min, max]를 벗어나는 경우 각각 min, max를 리턴하고, 그 외엔 num을 리턴한다.
+ * @param {Number} num
+ * @param {Number} min
+ * @param {Number} max
+ * @return {Number}
+ */
+exports.wrap = (num, min, max) => {
+  return Math.min(Math.max(num, min), max);
+};
+
+/**
+ * @description num이 [min, max] 범위 안에 있는지 리턴한다.
+ * @param {Number} num
+ * @param {Number} min
+ * @param {Number} max
+ * @return {Boolean}
+ */
+exports.isInRange = (num, min, max) => {
+  return num >= min && num <= max;
+};

lib/util_modules/string.js
@@ -1,0 +1,10 @@
+/**
+ * @description 문자 a에서 b까지 유니코드상의 거리를 리턴.
+ * @param {Char} a
+ * @param {Char} b
+ * @example charDistance('a', 'b') = 1, charDistance('c', 'b') = -1;
+ * @return {Number}
+ */
+exports.unicodeDistance = (a, b) => {
+  return b.charCodeAt(0) - a.charCodeAt(0);
+};

lib/utils.js
@@ -4,66 +4,14 @@
  * @see https://nodejs.org/api/fs.html#fs_file_system
  */
 
-const fs = require("fs");
-const fsPromises = require("fs/promises");
-const readline = require("readline");
+const input = require("./util_modules/input.js");
+const math = require("./util_modules/math.js");
+const string = require("./util_modules/string.js");
+const array = require("./util_modules/array.js");
 
-/**
- * @param {string} dir 읽을 파일 경루
- * @return {arrray[string]} 파일의 한 문장씩을 담은 문자열 배열
- */
-exports.inputByFile = async (dir) => {
-  const file = await fsPromises.readFile(dir, "utf-8");
-  return file.split(/\r?\n/); // window & mac
-};
-
-/**
- * @param {string} dir 읽을 파일 경로
- * @return {string} line 파일에서 순차적으로 한 줄씩 읽은 것
- * @example
- *  const reader = exports.lineReader("input.txt");
-    console.log(reader()); // 한줄씩 읽어온다
- */
-exports.lineReader = (dir) => {
-  const file = fs.readFileSync(dir, "utf8").split(/\r?\n/);
-  let line = 0;
-  return () => file[line++];
-};
-
-/**
- * @summary 콘솔에서 한 줄씩 입력받아 generator 안에 넣어주는 함수. {string}
- * @param {generator} solution 함수. yield로 input을 건네받는다
- * @return {void}
- * @see https://www.codecademy.com/articles/getting-user-input-in-node-js
- * @usage readConsole(generatorExample)
- * @example generatorExample 함수 참고
- */
-exports.readConsole = (generator) => {
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-
-  const gen = generator();
-  gen.next(); // to wait console input
-
-  rl.on("line", (line) => {
-    if (gen.next(line).done) rl.close();
-  });
-};
-
-const generatorExample = function* () {
-  const n = yield; // 콘솔에서 입력받은 한 줄 저장
-  const m = yield; // 콘솔에서 입력받은 한 줄 저장
-  return Number(n) + Number(m);
-};
-
-/**
- * @summary 파라미터로 받은 모든 수의 합을 구해 리턴한다
- * @param {array} 더할 수
- * @return {Number} 수의 합
- * @usage utils.sum(1,2,3,4)
- */
-exports.sum = (...nums) => {
-  return nums.reduce((prev, cur) => prev + cur, 0);
+module.exports = {
+  input,
+  math,
+  string,
+  array,
 };