2ca816d0aa8ca986df4a2652143ec29e5eeb9016
pyeonne <pyeonne@gmail.com>
2021-10-16 23:20:02 +0900

    docs(그리디): 이코테 11장 그리디 문제 1 풀이

---
+++ src/PART-03/11-Greedy/1.js
@@ -0,0 +1,37 @@
+* @name 모험가 길드
+* @link 동빈북 기출문제 11-1
+* @date 2021-10-16
+* @ref https://github.com/Sunmon/js-algorithm-basecamp/blob/dfs/bfs/dongbin-book/11-Greedy/01.js
+* @author pyeonne
+*/
+
+"use strict";
+
+const readConsole = require("../../../lib/utils").readConsole;
+
+const generator = function* () {
+ const n = yield;
+ const input = yield;
+ const data = input
+   .split(" ")
+   .map((i) => Number.parseInt(i))
+   .sort((a, b) => a - b);
+
+ console.log(data);
+ // 총 그룹의 수
+ let result = 0;
+
+ // 현재 그룹에 포함된 모험가의 수
+ let count = 0;
+
+ for (let i of data) {
+   count += 1;
+   if (count >= i) {
+     result += 1;
+     count = 0;
+   }
+ }
+ console.log(result);
+};
+
+readConsole(generator);
