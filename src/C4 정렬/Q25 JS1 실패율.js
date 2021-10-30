36a3bc942ad83f9d50476976915ac71e38c91bde
ganeodolu <ganeodolu@gmail.com>
2021-10-30 21:29:05 +0900

    refact: sort 수정

--- src/C4 정렬/Q25 JS1 실패율.js
+++ src/C4 정렬/Q25 JS1 실패율.js
@@ -1,10 +1,9 @@
-function solution(N, stages) {
-  let countStages = new Array(N + 1).fill(0);
+function solution(n, stages) {
+  let countStages = new Array(n + 1).fill(0);
   stages.forEach((val) => {
     countStages[val - 1]++;
   });
   let sumUsers = stages.length;
-
   const failureStages = [];
   countStages.forEach((val, idx) => {
     failureStages.push({
@@ -14,16 +13,11 @@ function solution(N, stages) {
     sumUsers -= val;
   });
   failureStages.pop();
-  failureStages.sort((a, b) => {
-    if (a.ratio < b.ratio) return 1;
-    if (a.ratio > b.ratio) return -1;
-    if (a.idx < b.idx) return -1;
-    if (a.idx > b.idx) return 1;
-  });
+  failureStages.sort((a, b) => b.ratio - a.ratio);
 
   return failureStages.map((val) => val.idx);
 }
 
 const N = 5;
-const stages = [2, 1, 2, 6, 2, 4, 3, 3];
-console.log(solution(N, stages));
+const STAGE = [2, 1, 2, 6, 2, 4, 3, 3];
+console.log(solution(N, STAGE));
